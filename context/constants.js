15ad12a85f02129d2f2ff2e78d9c2314ec48a369
zhangchun <zhangchun@oamicnet.com>
2024-09-02 16:38:22 +0800

    real-state-one

---
+++ context/constants.js
@@ -0,0 +1,113 @@
+import realEstate from "./RealEstate.json";
+
+export const REAL_ESTATE_ADDRESS = "0x9CFaF4a895F63D53D6E9f42dD005806FEBEFBED6";
+export const REAL_ESTATE_ABI = realEstate.abi;
+
+//PINATA KEYS
+export const PINATA_API_KEY = "8645ea6a763486f515bb";
+export const PINATA_SECRECT_KEY = "7c9537b50a1f501cfdd363c6032b8d58b4eccd90be748619cad2f514639d4906";
+
+//NETWORK
+const networks = {
+  polygon_amoy: {
+    chainId: `0x${Number(80002).toString(16)}`,
+    chainName: "Polygon Amoy",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc-amoy.polygon.technology/"],
+    blockExplorerUrls: ["https://www.oklink.com/amoy"],
+  },
+  polygon_mumbai: {
+    chainId: `0x${Number(80001).toString(16)}`,
+    chainName: "Polygon Mumbai",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc.ankr.com/polygon_mumbai"],
+    blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+  },
+  polygon: {
+    chainId: `0x${Number(137).toString(16)}`,
+    chainName: "Polygon Mainnet",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc.ankr.com/polygon"],
+    blockExplorerUrls: ["https://polygonscan.com/"],
+  },
+  bsc: {
+    chainId: `0x${Number(56).toString(16)}`,
+    chainName: "Binance Smart Chain Mainnet",
+    nativeCurrency: {
+      name: "Binance Chain Native Token",
+      symbol: "BNB",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc.ankr.com/bsc"],
+    blockExplorerUrls: ["https://bscscan.com"],
+  },
+  base_mainnet: {
+    chainId: `0x${Number(8453).toString(16)}`,
+    chainName: "Base Mainnet",
+    nativeCurrency: {
+      name: "ETH",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    rpcUrls: ["https://mainnet.base.org/"],
+    blockExplorerUrls: ["https://bscscan.com"],
+  },
+  base_sepolia: {
+    chainId: `0x${Number(84532).toString(16)}`,
+    chainName: "Base Sepolia",
+    nativeCurrency: {
+      name: "ETH",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    rpcUrls: ["https://sepolia.base.org"],
+    blockExplorerUrls: ["https://bscscan.com"],
+  },
+  localhost: {
+    chainId: `0x${Number(31337).toString(16)}`,
+    chainName: "localhost",
+    nativeCurrency: {
+      name: "LLC",
+      symbol: "LLC",
+      decimals: 18,
+    },
+    rpcUrls: ["http://127.0.0.1:8545/"],
+    blockExplorerUrls: ["https://bscscan.com"],
+  },
+};
+
+const changeNetwork = async ({ networkName }) => {
+  try {
+    if (!window.ethereum) throw new Error("No crypto wallet found");
+    await window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          ...networks[networkName],
+        },
+      ],
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+export const ACTIVE_NETWORK = "localhost";
+// export const ACTIVE_NETWORK = "polygon_amoy";
+
+export const handleNetworkSwitch = async () => {
+  const networkName = "localhost";
+  const network = await changeNetwork({ networkName });
+  return network;
+};
